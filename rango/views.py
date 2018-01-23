550fa64fa3c7d56836691629ea9a6e4545b321af
AllanPetrie <2254840P@student.gla.ac.uk>
2018-01-23 16:51:28 +0000

    23/01/18 (2)

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {"boldmessage": "Cruchy, creamy, cookie, candy, cupcake"}
-    return render(request, "rango/index.html", context = context_dict)
+    return render(request, "rango/index.html", context=context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, "rango/about.html", context=context_dict)
+    ##HttpResponse("Rango says this is the about page.")
 
 
 
