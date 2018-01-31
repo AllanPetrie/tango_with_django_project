cd5fad78697c97063c17fc9468f2dcdd9f97b626
Allan Petrie <2254840P@student.gla.ac.uk>
2018-01-31 14:06:03 +0000

    31/01/2018

--- rango/admin.py
+++ rango/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+
+    def __init__():
+        pass
+    
+    list_display = ("title","category","url")
+    
