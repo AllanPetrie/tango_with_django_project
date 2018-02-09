9dd1de2cf65db2ed9b01d744b6a28c43001b52b1
Allan Petrie <2254840P@student.gla.ac.uk>
2018-02-09 15:56:06 +0000

    09/02/18 (1)

--- rango/views.py
+++ rango/views.py
@@ -5,14 +5,25 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import  login_required
+from datetime import datetime
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by("-likes")[:5] #five most liked categories
     page_list = Page.objects.order_by("-views")[:5] #five most views pages
     context_dict = {"categories": category_list, "pages": page_list}
-    return render(request, "rango/index.html", context=context_dict)
+
+    response = render(request, "rango/index.html", context=context_dict)
+    visitor_cookie_handler(request, response)
+
+    return response
 
 def about(request):
+
+    if request.session.test_cookie_worked():
+        print("Test cookie worked!")
+        request.session.delete_test_cookie()
+
     return render(request, "rango/about.html", {})
 
 def show_category(request, category_name_slug):
@@ -134,3 +145,22 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    response.set_cookie('visit', visit)
